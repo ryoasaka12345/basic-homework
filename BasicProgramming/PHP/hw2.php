3a011f52cde84bddb7d58af2b84d311f8f2d3295
ryoasaka12345 <ryo.asaka@g.pascalia.co.jp>
2021-04-16 08:52:51 +0900

    revised hw1.php - hw4.php

--- BasicProgramming/PHP/hw2.php
+++ BasicProgramming/PHP/hw2.php
@@ -1,14 +1,15 @@
 <?php
 
+$n = 10;
+
 function factorialOfNumber($n)
 {
     $fact = 1;
-    for($i=$n; $i>0; $i--){
-        $fact *= $i;
-    }
+    for($i=$n; $i>0; $i--) $fact *= $i;
 
-    return $fact;
+    echo "$n! = $fact";
 }
 
-echo factorialOfNumber(10);
+factorialOfNumber($n);
+
 ?>
--- BasicProgramming/PHP/hw3.php
+++ BasicProgramming/PHP/hw3.php
@@ -1,19 +1,12 @@
 <?php
 
 function judgePrime($n){
-    for ($i=2; $i<=$n; $i++){
-        if ($n%$i==0){
-            break;
-        }
-    }
+    for ($i=2; $i<=$n; $i++) if ($n%$i==0) break;
     
-    if ($n==$i){
-        echo "$n is primenumber.";
-    }else{
-        echo "$n is not primenumber.";
-    }
+    if ($n==1 or $n==$i) echo "$n is primenumber. <br />";
+    else echo "$n is not primenumber. <br />";
 }
 
-judgePrime(11);
+for ($n=1; $n<=11; $n++) judgePrime($n);
 
 ?>

4d638cca9579f0fd9f2662c6104a6c311f2dbfb6
ryoasaka12345 <ryo.asaka@g.pascalia.co.jp>
2021-05-14 09:14:00 +0900

    On branch algorithm  Your branch is up to date with 'origin/algorithm'.
     Changes to be committed:
    	new file:   Algorithm/BinaryTree.php
    	modified:   Algorithm/Homework2.php
    	new file:   Algorithm/SearchBinaryTree.php

--- Algorithm/Homework2.php
+++ Algorithm/Homework2.php
@@ -1,23 +1,40 @@
 <?php
 
-require('QuickSort.php');
+require('SearchBinaryTree.php');
 
+function pushPairs($BST, $arr1, $arr2)
+{
+    foreach ($arr1 as $elem1){
+        foreach ($arr2 as $elem2){
+            $BST->insert([$elem1, $elem2]);
+        }
+    }
+}
+    
+// test 1
+$BST = new SearchBinaryTree();
 $arr1 = [11, 7, 1];
 $arr2 = [4, 6, 2];
-$result = [];
-
 $k = 3;
+pushPairs($BST, $arr1, $arr2);
+$BST->traverseInOrder($BST->getRoot(), $k); // output
 
-quicksort($arr1, 0, count($arr1) - 1);
-quicksort($arr2, 0, count($arr2) - 1);
+echo "<br>";
+// test 2
+$BST = new SearchBinaryTree();
+$arr1 = [4, 6, 2];
+$arr2 = [11, 7, 1];
+$k = 3;
+pushPairs($BST, $arr1, $arr2);
+$BST->traverseInOrder($BST->getRoot(), $k); // output
 
-for ($i = 0; $i < $k; $i++) {
-    $pair = [$arr1[0], $arr2[$i]];
-    array_push($result, $pair);
-}
-
-foreach ($result as $arr) {
-    echo "[", $arr[0], ", ", $arr[1], "], ";
-}
+echo "<br>";
+// test 3
+$BST = new SearchBinaryTree();
+$arr1 = [1, 2, 3, 4, 5];
+$arr2 = [6, 7, 8, 9, 10, 11, 12];
+$k = 5;
+pushPairs($BST, $arr1, $arr2);
+$BST->traverseInOrder($BST->getRoot(), $k); // output   
 
 ?>

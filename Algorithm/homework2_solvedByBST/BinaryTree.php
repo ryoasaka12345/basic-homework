2d8bcae171ea085447dd23946093e47b99436e32
ryoasaka12345 <ryo.asaka@g.pascalia.co.jp>
2021-05-18 17:16:27 +0900

    Changes to be committed: 	new file:   Algorithm/homework2_solvedByBST/BinaryTree.php 	new file:   Algorithm/homework2_solvedByBST/Homework2.php 	new file:   Algorithm/homework2_solvedByBST/SearchBinaryTree.php

---
+++ Algorithm/homework2_solvedByBST/BinaryTree.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+* A node of Binary Tree (BT)
+*/
+class Node {
+    /** @var int */
+    private $data;
+
+    /** @var Node left subtree */
+    private $left;
+
+    /** @var Node right subtree */
+    private $right;
+
+    public function __construct($data, $left = null, $right = null)
+    {
+        $this->data = $data;
+        $this->left = $left;
+        $this->right = $right;
+    }
+
+    /**
+    * get data
+    * @return int
+    */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+    * get sum
+    * @return int
+    */
+    public function getSum()
+    {
+        return $this->data[0] + $this->data[1];
+    }
+    
+    /**
+    * set data
+    * @param int $data
+    */
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+    * get left
+    * @return Node
+    */
+    public function getLeft()
+    {
+        return $this->left;
+    }
+
+    /**
+    * set left
+    * @param Node $left
+    */
+    public function setLeft($left)
+    {
+        $this->left = $left;
+    }
+
+    /**
+    * get right
+    * @return Node
+    */
+    public function getRight()
+    {
+        return $this->right;
+    }
+
+    /**
+    * set right
+    * @param Node $right
+    */
+    public function setRight($right)
+    {
+        $this->right = $right;
+    }
+}
+
+/**
+* Binary Tree Class
+*/
+class BinaryTree {
+    /** @var Node */ 
+    protected $root;
+
+    public function __construct($root = null)
+    {
+        $this->root = $root;
+    }
+
+	public function isEmpty() {
+        return $this->root === null;
+    }
+
+    /**
+    * get root
+    * @return Node
+    */
+    public function getRoot()
+    {
+        return $this->root;
+    }
+
+    /**
+    * set root
+    * @param Node $root
+    */
+    public function setRoot($root)
+    {
+        $this->root = $root;
+    }
+
+}
+
